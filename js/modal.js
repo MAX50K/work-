bc5696a2e54847902e9bd28d4d096eec4e46addf
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-06-09 18:55:06 +0200

    swsw

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,31 @@
-const openBtn = document.getElementById("open-modal-btn")
-const backdrop = document.getElementById("backdrop")
-const closeBtn = document.getElementById("modal__close__button")
+const openBtn = document.getElementById("open-modal-btn");
+const backdrop = document.getElementById("backdrop");
+const closeBtn = document.getElementById("modal__close__button");
 
-openBtn.addEventListener("click", toggleModal)
-closeBtn.addEventListener("click", toggleModal)
+openBtn.addEventListener("click", toggleModal);
+closeBtn.addEventListener("click", toggleModal);
 
 function toggleModal() {
-    backdrop.classList.toggle("active")
+  backdrop.classList.toggle("active");
+}
+
+const form = document.querySelector(".form");
+console.log(form);
+
+form.addEventListener("submit", onFormSubmit)
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.dir(event.currentTarget);
+}
+
+const jsInput = document.querySelector(".js-input")
+jsInput.addEventListener("focus", onInputFocus)
+jsInput.addEventListener("blur", onInputBlur)
+
+function onInputFocus() {
+    console.log("Інпут отримав фокус");
+}
+function onInputBlur() {
+    console.log("Інпут втратив фокус");
 }
